0c6a6ffd1248ab7215a38048d41c33d905087ae7
Mehernaz Savai <msavai@mathworks.com>
2023-02-03 15:49:04 -0800

    Updated Color Axis

--- 3-ChannelEstimation/3-HelperFiles/hPlotChEstimates.m
+++ 3-ChannelEstimation/3-HelperFiles/hPlotChEstimates.m
@@ -8,6 +8,8 @@ neural_mse = mean(abs(estChannelGridPerfect(:) - estChannelGridNN(:)).^2);
 interp_mse = mean(abs(estChannelGridPerfect(:) - interpChannelGrid(:)).^2);
 practical_mse = mean(abs(estChannelGridPerfect(:) - estChannelGrid(:)).^2);
 
+cmax = max(abs([estChannelGrid(:); estChannelGridNN(:); estChannelGridPerfect(:)]));
+
 figure
 
 subplot(1,4,1)
@@ -15,22 +17,26 @@ imagesc(abs(interpChannelGrid));
 xlabel('OFDM Symbol');
 ylabel('Subcarrier');
 title({'Linear Interpolation', ['MSE: ', num2str(interp_mse)]});
+clim([0 cmax]);
 
 subplot(1,4,2)
 imagesc(abs(estChannelGrid));
 xlabel('OFDM Symbol');
 ylabel('Subcarrier');
 title({'Practical Estimator', ['MSE: ', num2str(practical_mse)]});
+clim([0 cmax]);
 
 subplot(1,4,3)
 imagesc(abs(estChannelGridNN));
 xlabel('OFDM Symbol');
 ylabel('Subcarrier');
 title({'Neural Network', ['MSE: ', num2str(neural_mse)]});
+clim([0 cmax]);
 
 subplot(1,4,4)
 imagesc(abs(estChannelGridPerfect));
 xlabel('OFDM Symbol');
 ylabel('Subcarrier');
 title({'Actual Channel'});
+clim([0 cmax]);
 end
